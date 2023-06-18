80782e9765aa9c00a2d2d864233bee2dde1c8fdf
erenkibar <eren.kibar97@gmail.com>
2023-06-18 22:04:35 +0300

    Inital tables

---
+++ src/main/java/com/eren/carecommerce/CarEcommerceApplication.java
@@ -0,0 +1,31 @@
+package com.eren.carecommerce;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@SpringBootApplication
+public class CarEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarEcommerceApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI carEcommerceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Car E-commerce API")
+                        .description("API for the car e-commerce application")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+}
---
+++ src/main/java/com/eren/carecommerce/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.eren.carecommerce.repository;
+
+import com.eren.carecommerce.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
